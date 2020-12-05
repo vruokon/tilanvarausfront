357f9b374b09319e465c6c4bfa83902c18b64bf2
Viljami Ruokonen <vruokonen@gmail.com>
2020-12-05 18:55:51 +0200

    vue files for tilanvaraus front

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
+import ElementUi from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+import router from './router'
+
+Vue.use(ElementUi)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
