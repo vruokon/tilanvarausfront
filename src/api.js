bf37b46545f12c8c84d63a5f3b7543658dd2b2db
Viljami Ruokonen <vruokonen@gmail.com>
2020-12-05 21:59:56 +0200

    added login

---
+++ src/api.js
@@ -0,0 +1,5 @@
+const LOGIN = '/user/login'
+
+module.exports ={
+    LOGIN
+}
---
+++ src/views/Login.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <el-row type="flex" justify="center">
+      <el-col :span="10">
+        <el-form
+          label-position="left"
+          label-width="200px"
+          :model="registerForm"
+        >
+          <el-form-item label="Email">
+            <el-input type="email" v-model="registerForm.email"></el-input>
+          </el-form-item>
+          <el-form-item label="Password">
+            <el-input
+              type="password"
+              v-model="registerForm.password"
+            ></el-input>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+    <el-row type="flex" justify="end">
+      <el-col :span="10">
+        <el-button type="primary" @click="login">Login</el-button>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import { LOGIN } from "../api";
+export default {
+  name: "Login",
+  data() {
+    return {
+      registerForm: {
+        email: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    login() {
+      axios
+        .post(`http://127.0.0.1:5000${LOGIN}`, {
+          email: this.registerForm.email,
+          password: this.registerForm.password,
+        })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      console.log(LOGIN);
+    },
+  },
+};
+</script>
