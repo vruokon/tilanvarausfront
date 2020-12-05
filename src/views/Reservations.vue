e59dc5864ee843c17bd3dae520d85573234cc227
Viljami Ruokonen <vruokonen@gmail.com>
2020-12-05 22:00:15 +0200

    reservations

---
+++ src/views/Reservations.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">from here you can make reservations</div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Reservations",
+  components: {},
+};
+</script>
