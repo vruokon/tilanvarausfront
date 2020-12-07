89689590385ada33acac60b87a820740d234dd2a
Viljami Ruokonen <vruokonen@gmail.com>
2020-12-07 19:02:40 +0200

    added vuex store

--- src/main.js
+++ src/main.js
@@ -3,12 +3,21 @@ import ElementUi from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 import router from './router'
+import Vuex from 'vuex'
 
+Vue.use(Vuex)
 Vue.use(ElementUi)
 
 Vue.config.productionTip = false
 
+const store = new Vuex.Store({
+  state: {
+    user: null
+  }
+})
+
 new Vue({
   router,
+  store: store,
   render: h => h(App)
 }).$mount('#app')
